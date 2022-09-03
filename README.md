100298aaf24f5324cbbab7df2a2055bb53a47bc7
ivandjoh <ivandi.djoh@gmail.com>
2022-09-03 14:28:37 +0800

    first init

---
+++ README.md
@@ -0,0 +1,9 @@
+## Spring-Mongo
+
+### About
+This is a small apps that uses Spring Boot and MongoDB to create a simple REST API.
+
+### How to run
+1. Install MongoDB
+2. Run `mvn spring-boot:run`
+3. Open `http://localhost:1000` in your browser
---
+++ src/test/java/ivandjoh/online/springmongo/SpringMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package ivandjoh.online.springmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
